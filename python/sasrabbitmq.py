53f9f03d28f4c0f41b34d07bbd0f2c77fd235885
Mudit Mishra <mudit.mishra@sas.com>
2018-04-12 00:17:49 +1000

    add delete support

--- python/sasrabbitmq.py
+++ python/sasrabbitmq.py
@@ -24,61 +24,40 @@ try:
 except NameError:
     to_unicode = str
 
+MESSAGE_FILE_PREFIX = "message"
 LOG_FORMAT = ('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
 
-def getInputParameters():
 
-        LOGGER = logging.getLogger('getInputParameters')
-        parser = argparse.ArgumentParser(prog='sasrabbitmq.py', usage='%(prog)s [options]')
-
-        requiredNamed = parser.add_argument_group('Required arguments')
-        requiredNamed.add_argument('-receive',
-                                   action="store_true",
-                                   help='Receive message from RMQ without acknowledgement')
-        requiredNamed.add_argument('-ack',
-                                   action="store_true",
-                                   help='Acknowledge message from RMQ without acknowledgement')
-        requiredNamed.add_argument('-u', '--uri',
-                                   dest='uri',
-                                   help="URI needed by pikka module to connect to RabbitMQ server e.g. amqp://username:password@rabbitmq.mydomain.com:5672/%%2F",
-                                   type=str,
-                                   required=True)
-        requiredNamed.add_argument('-q', '--queue',
-                                   dest='queue',
-                                   help='Name of RabbitMQ queue',
-                                   type=str,
-                                   required=True)
-        requiredNamed.add_argument('-o', '--output','-i', '--input',
-                                    dest='file',
-                                    help='Output or Input file where messages are stored',
-                                    required=True,
-                                    type=str
-                                    )
-
-        return parser.parse_args()
 
 class RMQConsumer(object):
 
-    def __init__(self, amqp_url):
-        """Create a new instance of the consumer class, passing in the AMQP
-        URL used to connect to RabbitMQ.
+    def __init__(self, amqp_uri,action,queue,filename):
 
-        :param str amqp_url: The AMQP url to connect with
+        LOGGER = logging.getLogger('RMQConsumer.initialize')
+        self._url = amqp_uri
+        self._action = action
+        self._queuename = queue
+        self._ack = False
 
-        """
+        self.filename = filename
         self._connection = None
         self._channel = None
         self._closing = False
         self._consumer_tag = None
-        self._url = amqp_url
-        self._queuename = 'tripwireapplication.#'
         self._queue_state = None
         self._queue_size = 0
         self._messages_fetch_limit = 2
         self._message_consume_counter = 0
         self._messages_dict = []
         self._consumer_tag = None
+        self._localInMemoryMessageIds = []
+
+        if action.lower() == 'delete':
+            #Remove Messages in RabbitMQ by consume and acknowledgement
+            self._ack = True
+            LOGGER.info("Loading Messages ids from Text File %s" % filename)
+            self._localInMemoryMessageIds = self.loadMsgIdFromTextFile(filename)
 
     def connect(self):
         LOGGER = logging.getLogger('RMQConsumer.connect')
@@ -127,8 +106,10 @@ class RMQConsumer(object):
             prefetch_limit=self.getFetchLimit()
             if prefetch_limit:
                 self._channel.basic_qos(prefetch_count=self.getFetchLimit())
-                self._consumer_tag = self._channel.basic_consume(self.on_message,
-                                  queue=self._queuename)
+                if not _ack:
+                    self._consumer_tag = self._channel.basic_consume(self.on_message,queue=self._queuename)
+                else:
+                    self._consumer_tag = self._channel.basic_consume(self.on_message,queue=self._queuename)
                 self._channel.start_consuming()
             else:
                 self.close_connection()
@@ -164,11 +145,27 @@ class RMQConsumer(object):
 
     def on_message(self,ch, method, properties, body):
         LOGGER = logging.getLogger('RMQConsumer.on_message')
-        self._message_consume_counter += 1
-        self._messages_dict.append(json.loads(body))
-        if self._message_consume_counter == self._messages_fetch_limit:
-            self.stop_consuming()
-            self.close_connection()
+        message_body = json.loads(body)
+        if not _ack:
+            self._message_consume_counter += 1
+            self._messages_dict.append(message_body)
+            if self._message_consume_counter == self._messages_fetch_limit:
+                self.stop_consuming()
+                self.close_connection()
+        else:
+            id = message_body['id']
+            if self.checkMessageExist(id):
+                self.acknowledge_message(method.delivery_tag)
+
+    def acknowledge_message(self, delivery_tag):
+        LOGGER.info('Acknowledging message %s', delivery_tag)
+        self._channel.basic_ack(delivery_tag)
+
+    def checkMessageExist(self,id):
+        if id in self._localInMemoryMessageIds:
+            return True
+        else:
+            return False
 
     def save_messages(self):
         LOGGER = logging.getLogger('RMQConsumer.save_messages')
@@ -176,8 +173,27 @@ class RMQConsumer(object):
         with open('messages_%s.json' % (timestr), 'w') as outfile:
             json.dump(self._messages_dict, outfile, indent = 4)
 
-    def run(self):
+    def loadMsgIdFromTextFile(self,in_text_file):
+        LOGGER = logging.getLogger('Consumer.loadLocalMessageIds')
+        localInMemoryMessageIds = []
+        try:
+            for line in in_text_file.readlines():
+                LOGGER.debug("Acknowledging Message %s" % line)
+                localInMemoryMessageIds.append(line)
+        except Exception as err:
+            LOGGER.error(err)
+            localInMemoryMessageIds = []
+
+        return localInMemoryMessageIds
+
+    def run(self,action, queue, filename):
+        LOGGER = logging.getLogger('RMQConsumer.run')
         self._message_consume_counter = 0
+        if action.lower() == 'delete':
+            LOGGER.info("To remove message from RabbitMQ consume and Acknowledge messages")
+            self._ack = True
+            LOGGER.info("Loading Messages ids from Text File %s" % filename)
+            self.loadMsgIdFromTextFile(filename)
         self.connect()
         self.open_channel()
         self.setup_queue()
@@ -185,31 +201,45 @@ class RMQConsumer(object):
         self.save_messages()
 
 
+def getArguments():
+        LOGGER = logging.getLogger('getInputParameters')
+        parser = argparse.ArgumentParser(prog='sasrabbitmq.py', usage='%(prog)s [options]')
+        requiredNamed = parser.add_argument_group('Required arguments')
+        requiredNamed.add_argument('-receive',action="store_true",help='Receive message from RMQ without acknowledgement')
+        requiredNamed.add_argument('-delete',action="store_true", help='Acknowledge message from RMQ without acknowledgement')
+        requiredNamed.add_argument('-u', '--uri',dest='uri',help="URI needed by pikka module to connect to RabbitMQ server e.g. amqp://username:password@rabbitmq.mydomain.com:5672/%%2F",type=str,required=True)
+        requiredNamed.add_argument('-q', '--queue',dest='queue',help='Name of RabbitMQ queue',type=str,required=True)
+        requiredNamed.add_argument('-o', '--output','-i', '--input',dest='file',help='Output or Input file where messages are stored',required=True,type=str)
+        args_dict = vars(parser.parse_args())
+
+        if args_dict['receive']:
+            args_dict['action'] = 'receive'
+        elif args_dict['delete']:
+            args_dict['action'] = 'delete'
+        else:
+            args_dict['action']='NA'
+        return args_dict
 
 def main(argv):
-
+    #initialize logging
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
     LOGGER = logging.getLogger('sasrabbitmq')
 
-    results = getInputParameters()
-    if results.receive:
-        action='receive'
-    elif results.ack:
-        action='ack'
-    else:
-        action='NA'
-
-    amqp_uri = results.uri
-    filename = results.file
-    queue = results.queue
+    #Read input options
+    args_dict = getArguments()
+    action = args_dict['action']
+    amqp_uri = args_dict['uri']
+    filename = args_dict['file']
+    queue = args_dict['queue']
 
     if 'amqp://' in amqp_uri:
         try:
-            #consumer.run(action, amqp_uri, queue, filename)
             LOGGER.info("Action: %s" % action)
             LOGGER.info("URI: %s" % amqp_uri)
             LOGGER.info("Queue: %s" % queue)
             LOGGER.info("Filename: %s" % filename)
+            #consumer = Consumer(amqp_uri,action,queue,filename)
+            #consumer.run()
         except KeyboardInterrupt:
             #consumer.stop()
             LOGGER.info('Stop')
@@ -217,6 +247,7 @@ def main(argv):
         LOGGER.info("Invalid AMQP URI: %s" % amqp_uri)
 
     return 0
+
 
 if __name__ == '__main__':
     try:
